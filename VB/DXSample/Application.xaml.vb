b4dd8fd07f2d1e3b38697e90e7431be53052db95
DevExpressExampleBot <fakeEmail>
2018-11-08 11:44:53 +0300

    Source auto update [skip ci]

---
+++ VB/DXSample/Application.xaml.vb
@@ -0,0 +1,22 @@
+﻿' Developer Express Code Central Example:
+' How to implement the ThemeMananger theme support in custom controls
+' 
+' This is a sample project for the K18542 (http://www.devexpress.com/scid=K18542)
+' KB article illustrating how to support themes in custom controls.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E3524
+
+Imports System.Windows
+
+Namespace DXSample
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Partial Public Class App
+        Inherits Application
+
+        Public Sub New()
+        End Sub
+    End Class
+End Namespace
---
+++ VB/DXSample/MainWindow.xaml.vb
@@ -0,0 +1,27 @@
+﻿' Developer Express Code Central Example:
+' How to implement the ThemeMananger theme support in custom controls
+' 
+' This is a sample project for the K18542 (http://www.devexpress.com/scid=K18542)
+' KB article illustrating how to support themes in custom controls.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E3524
+
+Imports DevExpress.Xpf.Core
+
+Namespace DXSample
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits System.Windows.Window
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub comboBoxEdit1_EditValueChanged(ByVal sender As Object, ByVal e As DevExpress.Xpf.Editors.EditValueChangedEventArgs)
+            ApplicationThemeHelper.ApplicationThemeName = TryCast(e.NewValue, String)
+        End Sub
+    End Class
+End Namespace
