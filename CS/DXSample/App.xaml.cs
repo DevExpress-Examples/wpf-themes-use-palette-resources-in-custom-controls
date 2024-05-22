b4cbbcda5624562e81cc08f45e12ed10b4f76fb5
AndreySlabov <andrey.slabov@devexpress.com>
2024-05-22 14:39:47 +0400

    Lightweight Theme Resources

--- CS/DXSample/App.xaml.cs
+++ CS/DXSample/App.xaml.cs
@@ -1,20 +1,10 @@
-﻿// Developer Express Code Central Example:
-// How to implement the ThemeMananger theme support in custom controls
-// 
-// This is a sample project for the K18542 (http://www.devexpress.com/scid=K18542)
-// KB article illustrating how to support themes in custom controls.
-// 
-// You can find sample updates and versions for different programming languages here:
-// http://www.devexpress.com/example=E3524
-
+﻿using DevExpress.Xpf.Core;
 using System.Windows;
 
 namespace DXSample {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application {
         public App() {
+            CompatibilitySettings.UseLightweightThemes = true;
         }
     }
 }
--- CS/DXSample/CustomControls.cs
+++ CS/DXSample/CustomControls.cs
@@ -1,7 +1,5 @@
-﻿using System.Reflection;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using DevExpress.Xpf.Utils.Themes;
 
 namespace DXSample {
     public class CustomControl1 : Control {
@@ -9,43 +7,11 @@ namespace DXSample {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomControl1), new FrameworkPropertyMetadata(typeof(CustomControl1)));
         }
 
-        public string Text
-        {
+        public string Text {
             get { return (string)GetValue(TextProperty); }
             set { SetValue(TextProperty, value); }
         }
         public static readonly DependencyProperty TextProperty =
             DependencyProperty.Register("Text", typeof(string), typeof(CustomControl1), new PropertyMetadata(null));
-    }
-
-    public class CustomControl2 : CustomControl1 {
-        static CustomControl2() {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomControl2), new FrameworkPropertyMetadata(typeof(CustomControl2)));
-        }
-    }
-
-    public class CustomControl3 : CustomControl1 {
-        static CustomControl3() {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomControl3), new FrameworkPropertyMetadata(typeof(CustomControl3)));
-        }
-    }
-
-    public class CustomControl4 : CustomControl1 {
-        static CustomControl4() {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomControl4), new FrameworkPropertyMetadata(typeof(CustomControl4)));
-        }
-    }
-
-    public class ThemeResourcesThemeKeyExtension : ThemeKeyExtensionBase<ThemeResourcesThemeKeys> {
-        protected override Assembly GetAssembly() {
-            return TypeInTargetAssembly != null ? TypeInTargetAssembly.Assembly : GetType().Assembly;
-        }
-    }
-    public enum ThemeResourcesThemeKeys {
-        Background,
-        Foreground,
-        BorderBrush,
-        BorderThickness,
-        FontSize
     }
 }
