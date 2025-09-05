2144bb9971214201a6bcaafb4c665501b845b247
DevExpressExampleBot <fakeEmail>
2025-09-05 12:47:24 +0400

    Source auto update [skip ci]

--- VB/DXSample/MainWindow.xaml.vb
+++ VB/DXSample/MainWindow.xaml.vb
@@ -1,20 +1,9 @@
-' Developer Express Code Central Example:
-' How to implement the ThemeMananger theme support in custom controls
-' 
-' This is a sample project for the K18542 (http://www.devexpress.com/scid=K18542)
-' KB article illustrating how to support themes in custom controls.
-' 
-' You can find sample updates and versions for different programming languages here:
-' http://www.devexpress.com/example=E3524
 Imports DevExpress.Xpf.Core
 
 Namespace DXSample
 
-    ''' <summary>
-    ''' Interaction logic for MainWindow.xaml
-    ''' </summary>
     Public Partial Class MainWindow
-        Inherits System.Windows.Window
+        Inherits ThemedWindow
 
         Public Sub New()
             Me.InitializeComponent()
