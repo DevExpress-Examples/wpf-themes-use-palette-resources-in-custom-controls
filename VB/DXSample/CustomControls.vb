a2babf4ffaa703757353274b4908ce87aa3f4a1d
DevExpressExampleBot <fakeEmail>
2026-01-30 14:52:31 +0400

    Source auto update [skip ci]

--- VB/DXSample/CustomControls.vb
+++ VB/DXSample/CustomControls.vb
@@ -1,7 +1,5 @@
-Imports System.Reflection
 Imports System.Windows
 Imports System.Windows.Controls
-Imports DevExpress.Xpf.Utils.Themes
 
 Namespace DXSample
 
@@ -24,44 +22,4 @@ Namespace DXSample
 
         Public Shared ReadOnly TextProperty As DependencyProperty = DependencyProperty.Register("Text", GetType(String), GetType(CustomControl1), New PropertyMetadata(CType(Nothing, PropertyChangedCallback)))
     End Class
-
-    Public Class CustomControl2
-        Inherits CustomControl1
-
-        Shared Sub New()
-            Call DefaultStyleKeyProperty.OverrideMetadata(GetType(CustomControl2), New FrameworkPropertyMetadata(GetType(CustomControl2)))
-        End Sub
-    End Class
-
-    Public Class CustomControl3
-        Inherits CustomControl1
-
-        Shared Sub New()
-            Call DefaultStyleKeyProperty.OverrideMetadata(GetType(CustomControl3), New FrameworkPropertyMetadata(GetType(CustomControl3)))
-        End Sub
-    End Class
-
-    Public Class CustomControl4
-        Inherits CustomControl1
-
-        Shared Sub New()
-            Call DefaultStyleKeyProperty.OverrideMetadata(GetType(CustomControl4), New FrameworkPropertyMetadata(GetType(CustomControl4)))
-        End Sub
-    End Class
-
-    Public Class ThemeResourcesThemeKeyExtension
-        Inherits ThemeKeyExtensionBase(Of ThemeResourcesThemeKeys)
-
-        Protected Overrides Function GetAssembly() As Assembly
-            Return If(TypeInTargetAssembly IsNot Nothing, TypeInTargetAssembly.Assembly, [GetType]().Assembly)
-        End Function
-    End Class
-
-    Public Enum ThemeResourcesThemeKeys
-        Background
-        Foreground
-        BorderBrush
-        BorderThickness
-        FontSize
-    End Enum
 End Namespace
